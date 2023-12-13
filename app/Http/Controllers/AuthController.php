98d72a7f411639252b4de382ffc1a11c9f6d007a
siposromeo <sipos.romeo.bence@students.jedlik.eu>
2023-12-13 12:25:10 +0100

    69

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,13 +4,24 @@ namespace App\Http\Controllers;
 // php artisan make:controller AuthController
 
 use App\Models\User;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 
 class AuthController extends BaseController
 {
     public function register(Request $request){
+
+        $validator = Validator::make($request -> all(), [
+            'name' => 'required'
+        ]);
+
         $input = $request ->all();
+        $input['password'] = bcrypt($input['password']);
         $user = User::create($input);
+
+        $success['name'] = $user -> name;
+        $success['token'] = $user -> createToken('Secret') -> plainTextToken;
+
         return $this -> sendResponse('','Sikeres regisztáció!');
     }
 }
